f8279215f66254d54f00189ebba0ecd7a822623a
Rohan2821999 <rohanhundia@gmail.com>
2016-08-02 11:47:53 -0700

    Added normalized RdGrYl color map to P(Acc) Data

--- MathCog_Modelling/Plotting_P(Acc).py
+++ MathCog_Modelling/Plotting_P(Acc).py
@@ -1,6 +1,7 @@
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.pylab as pl
+from matplotlib import cm
 import numpy as np
 import math
 
@@ -15,6 +16,9 @@ w = 0.15 # weber fraction values
 numerator = abs(n1-n2)
 denominator = math.sqrt(2)*w*(((n1**2)+(n2**2))**0.5)
 
+cmap = cm.RdYlGn
+c = cm.ScalarMappable(cmap=cmap, norm = mpl.colors.Normalize(vmin=0.5,vmax=1))
+
 #print(numerator)
 #print numerator[2]
 
@@ -22,7 +26,8 @@ for i in xrange (len(numerator)):
     P_Error = 0.5*math.erfc(numerator[i]/denominator[i])
     #print(P_Error)
     P_Acc = 1 - P_Error
-    plt.scatter([ratio[i]], [n1[i]], color=[(P_Acc),(P_Acc),(P_Acc)])
+    colors = c.to_rgba(P_Acc)
+    plt.scatter([ratio[i]], [n1[i]], color = colors)
 
 
 #print(P_Acc)
