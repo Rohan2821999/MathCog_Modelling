dd5ba961a100192bf56f3200aee70c658018354f
Rohan2821999 <rohanhundia@gmail.com>
2016-08-08 12:04:45 -0700

    Optimized P_Acc Code

--- MathCog_Modelling/Plotting_P(Acc).py
+++ MathCog_Modelling/Plotting_P(Acc).py
@@ -5,17 +5,12 @@ from matplotlib import cm
 import numpy as np
 import math
 
-P_Error = []
-BinaryBns = []
-P_Acc = []
-numerator = []
-denominator = []
-
 
 data = np.genfromtxt(r'data_sim.csv',delimiter = ',' ,names = True, dtype = None,)
 ratios = list(set(data['ratio']))
-n1s = list(set(data['n1']))
-n2s = list(set(data['n2']))
+n1s = (data['n1'])
+n2s = (data['n2'])
+ns = []
 
 w = 0.15 # weber fraction values
 cmap = cm.RdYlGn
@@ -24,20 +19,24 @@ c = cm.ScalarMappable(cmap=cmap, norm = mpl.colors.Normalize(vmin=0.5,vmax=1))
 #print(numerator)
 #print numerator[2]
 
-for ratio in ratios:
-    for n1 in n1s:
-        print(n1)
-        #numerator = abs(n1-n2)
-        #denominator = (math.sqrt(2)*w*(((n1**2)+(n2**2))**0.5))
-        #P_Error = 0.5*math.erfc(numerator/denominator)
-        #P_Acc = 1 - P_Error
-        colors = c.to_rgba(P_Acc)
-        plt.scatter([ratio], [n1],color = colors)
+ns = []
+for n1,n2 in zip(n1s,n2s):
+    temp = [n1, n2]
+    if temp not in ns:
+        ns.append([n1,n2])
 
 
-#plt.scatter([ratios], [n1s])
-#print(P_Acc)
-#plt.scatter(ratio,P_Acc)
+for n1,n2 in ns:
+    ratio = float(n1)/n2
+    numerator = (abs(n1-n2))
+    denominator = (math.sqrt(2)*w*(((n1**2)+(n2**2))**0.5))
+    P_Error = (0.5*math.erfc(numerator/denominator))
+    P_Acc = (1 - P_Error)
+    print(P_Acc)
+    colors = c.to_rgba(P_Acc)
+    plt.scatter([ratio], [n1],color = colors)
+
+
 plt.xlabel('Ratio (n1/n2)')
 plt.ylabel('n1')
 plt.show()
