dcdfc8e1d17977145226c79dbeb9ea0a9f874cdd
Rohan2821999 <rohanhundia@gmail.com>
2016-08-09 13:37:48 -0700

    Object oriented the current code. Improvements still to be made.. Changes to be made to Easiness_Space Graph

--- MathCog_Modelling/Plotting_P(Acc).py
+++ MathCog_Modelling/Plotting_P(Acc).py
@@ -10,32 +10,66 @@ data = np.genfromtxt(r'data_sim.csv',delimiter = ',' ,names = True, dtype = None
 ratios = list(set(data['ratio']))
 n1s = (data['n1'])
 n2s = (data['n2'])
+Reaction_T = (data['RT'])
 ns = []
+Acc_array = []
+ratio = []
+numerator,denominator,P_Error,P_Acc,array_possible,vals = [],[],[],[],[],[]
 
-w = 0.15 # weber fraction values
+w = 0.15
 cmap = cm.RdYlGn
-c = cm.ScalarMappable(cmap=cmap, norm = mpl.colors.Normalize(vmin=0.5,vmax=1))
-
-#print(numerator)
-#print numerator[2]
 
 ns = []
-for n1,n2 in zip(n1s,n2s):
-    temp = [n1, n2]
+RT_ns, n1_ns, n2_ns = [],[],[]
+
+for n1,n2,RT in zip(n1s,n2s,Reaction_T):
+    temp = [n1, n2, RT]
     if temp not in ns:
-        ns.append([n1,n2])
+        ns.append([n1,n2,RT])
+        RT_ns.append(RT)
+        n1_ns.append(n1)
+        n2_ns.append(n2)
+
+for n1,n2,RT in ns:
+    ratio.append(float(n1)/n2)
+    numerator.append(abs(n1-n2))
+    denominator.append(math.sqrt(2)*w*(((n1**2)+(n2**2))**0.5))
+    P_Error.append(0.5*math.erfc(numerator[-1]/denominator[-1]))
+    P_Acc.append(1 - P_Error[-1])
+    array_possible.append(np.random.choice([0,1],size=(10),p=[1-P_Acc[-1],P_Acc[-1]]))
+    vals.append(np.random.choice(array_possible[-1]))
 
 
-for n1,n2 in ns:
-    ratio = float(n1)/n2
-    numerator = (abs(n1-n2))
-    denominator = (math.sqrt(2)*w*(((n1**2)+(n2**2))**0.5))
-    P_Error = (0.5*math.erfc(numerator/denominator))
-    P_Acc = (1 - P_Error)
-    print(P_Acc)
-    colors = c.to_rgba(P_Acc)
-    plt.scatter([ratio], [n1],color = colors)
+def Plot_BinnedDiff(c = cm.ScalarMappable(cmap=cmap, norm = mpl.colors.Normalize(vmin=0.5,vmax=1))):
+    for i in xrange(len(RT_ns)):
+            if RT_ns[i]< 2000:
+                if vals[i] == 0:
+                    vals[i] = 0.5
+                colors = c.to_rgba(vals[i])
+                plt.scatter([ratio[i]], [n1_ns[i]],color = colors)
 
+def Plot_E (c = cm.ScalarMappable(cmap=cmap, norm = mpl.colors.Normalize(vmin=-1,vmax=1))):
+    for i in xrange(len(RT_ns)):
+        if RT_ns[i]<2000:
+            E = (vals[i]-(RT_ns[i]/2000.0))
+            colors = c.to_rgba(E)
+            plt.scatter([ratio[i]], [n1_ns[i]],color = colors)
+'''
+def Plot_E_Space(c = cm.ScalarMappable(cmap=cmap, norm = mpl.colors.Normalize(vmin=-1,vmax=1))):
+    for r in ratios:
+        for i in xrange(len(RT_ns)):
+            if RT_ns[i]<2000:
+                E = (vals[i]-(RT_ns[i]/2000.0))
+                colors = c.to_rgba(E)
+                plt.scatter([r], [n1_ns[i]],color = colors)
+'''
+def Plot_PAcc(c = cm.ScalarMappable(cmap=cmap, norm = mpl.colors.Normalize(vmin=0.5,vmax=1))):
+    for i in xrange(len(P_Acc)):
+        if RT_ns[i]< 2000:
+            colors = c.to_rgba(P_Acc[i])
+            plt.scatter([ratio[i]], [n1_ns[i]],color = colors)
+
+Plot_PAcc()
 
 plt.xlabel('Ratio (n1/n2)')
 plt.ylabel('n1')
--- MathCog_Modelling/Plotting_RT.py
+++ MathCog_Modelling/Plotting_RT.py
@@ -14,55 +14,38 @@ n1s = list(set(data['n1']))
 Reaction_T = data['RT']
 distances  = abs(data['n1'] - data['n2'])
 
+y=np.poly1d(np.polyfit(distances,Reaction_T,1))(distances) # Generating equation form
 
+def Plot_RT_Regression():
+    for i in xrange (len(Reaction_T)):
+        if(Reaction_T[i])<=2000:
+            plt.scatter(distances[i],Reaction_T[i])
 
-'''
-for i in xrange (len(Reaction_T)):
-    if(Reaction_T[i])<=2000:
-        plt.scatter(distance[i],Reaction_T[i])
+    plt.plot(distances,y,color='r')
+    plt.xlabel('Distance (abs(n1-n2))')
+    plt.ylabel('Reaction Time')
+    plt.show()
 
-y=np.poly1d(np.polyfit(distance,Reaction_T,1))(distance)
-plt.plot(distance,y,color='r')
-plt.xlabel('Distance (abs(n1-n2))')
-plt.ylabel('Reaction Time')
-plt.show()
-
-
-def slope_Yintercept(Y2=y[70],Y1=y[0],X2=distance[70],X1=distance[0]):
+def slope_Yintercept(Y2=y[70],Y1=y[0],X2=distances[70],X1=distances[0]):
     m = ((Y2-Y1)/(X2-X1))
     c = Y2 - (m*X2)
-    print(m,c)
+    return(m,c)
+    #print(m,c)
 
-#df = pd.read_csv('data_sim.csv')
-#unique_distance = list(set(distance))
-'''
+def NormalizedRT_map():
+    cmap = cm.RdYlGn
+    m = slope_Yintercept()[0]
+    intercept = slope_Yintercept()[1]
+    c = cm.ScalarMappable(cmap=cmap, norm = mpl.colors.Normalize(vmin=500,vmax=2500))
+    for ratio in ratios:
+        for n1 in n1s:
+            dist = n1 - int(n1 / ratio)
+            RT = (dist * m) + intercept
+            val = np.random.normal(RT,370)
+            colors = c.to_rgba(val)
+            plt.scatter([ratio],[n1],color = colors)
 
-#print(RT_dis)
-Std = np.std(RT_dis)
-Mean = np.mean(RT_dis)
-
-List_Mean = [1102,980,786,1046,1009,850,860,821,850,817]
-
-print(Std,Mean)
-#slope_Yintercept()
-
-cmap = cm.RdYlGn
-c = cm.ScalarMappable(cmap=cmap, norm = mpl.colors.Normalize(vmin=500,vmax=2500))
-for ratio in ratios:
-    for n1 in n1s:
-        dist = int(n1 / ratio)
-        RT = (dist * -12.6) + 1097
-        val = np.random.normal(RT,370)
-        colors = c.to_rgba(val)
-        plt.scatter([ratio],[n1],color = colors)
-
-plt.xlabel('Ratio')
-plt.ylabel('n1')
-plt.show()
-
-#for dist in unique_distance:
-#    RT_dis = df[df[unique_distance==dist]]['RT']
-
-    #print(distance)
-    #RT_dis = data[data[unique_distance]==distance]['RT']
-    #print(RT_dis)
+    plt.xlabel('Ratio')
+    plt.ylabel('n1')
+    plt.show()
+NormalizedRT_map()
