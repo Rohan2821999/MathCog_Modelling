61b993db56230f6980cf6274c342b14dbe9d0c9c
Rohan2821999 <rohanhundia@gmail.com>
2016-08-11 17:49:29 -0700

    Added Parameter of Guessing

--- MathCog_Modelling/Easiness_Map.py
+++ MathCog_Modelling/Easiness_Map.py
@@ -1,4 +1,3 @@
-
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.pylab as pl
@@ -10,6 +9,7 @@ import math
 data = np.genfromtxt(r'data_sim.csv',delimiter = ',' ,names = True, dtype = None,)
 ratios = list(set(data['ratio']))
 n1s_set = list(set(data['n1']))
+
 
 cmap = cm.RdYlGn
 
@@ -38,19 +38,37 @@ class Person:
       E = val - (rt/2000.)
       return E
 
-Child = Person(0.2,-12.7,1097)
-Adult = Person(0.15,-20,1100)
+Child1 = Person(0.2,-12.7,1097)
+Child2 = Person(0.25,-12.7,1097)
+Child3 = Person(0.28,-12.7,1097)
+Child4 = Person(0.18,-12.7,1097)
 
+
+Adult1 = Person(0.15,-20,1100)
+Adult2 = Person(0.13,-12.7,1097)
+Adult3 = Person(0.13,-12.7,1500)
+Adult4 = Person(0.10,-20,1100)
+
+
+Subjects = [Child1,Child2,Child3,Child4,Adult1,Adult2,Adult3,Adult4]
+
+#print(type(Guessing_Paramater))
+P_Guessing = 0.1
 def Plot_E_Space():
-    for ratio in ratios:
-        for n1 in n1s_set:
-            Easiness = Child.Ea(n1,ratio)
-            c = cm.ScalarMappable(cmap=cmap, norm = mpl.colors.Normalize(vmin=-1,vmax=1))
-            colors = c.to_rgba(Easiness)
-            plt.scatter([ratio], [n1],color = colors)
+    counter = 0
+    for i in xrange(len(Subjects)):
+        #print(i)
+        for ratio in ratios:
+            for n1 in n1s_set:
+                List_Functions = [-1,Subjects[i].Ea(n1,ratio)]
+                Easiness = (np.random.choice(List_Functions,p=[P_Guessing,0.9]))
+                c = cm.ScalarMappable(cmap=cmap, norm = mpl.colors.Normalize(vmin=-1,vmax=1))
+                colors = c.to_rgba(Easiness)
+                plt.figure(i+1)
+                plt.scatter([ratio], [n1],color = colors)
+        plt.xlabel('Ratio (n1/n2)')
+        plt.ylabel('n1')
+        plt.savefig("Easiness Plot- Subject "+str(i+1))
 
 Plot_E_Space()
-
-plt.xlabel('Ratio (n1/n2)')
-plt.ylabel('n1')
 plt.show()
